e61a6c9a1f2168428b8c92ecbb4957832671729a
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-12-16 14:56:00 -0800

    Add tests for divzero.

--- WoodworkingCalculatorTests/WoodworkingCalculatorParserTests.swift
+++ WoodworkingCalculatorTests/WoodworkingCalculatorParserTests.swift
@@ -10,6 +10,7 @@ struct WoodworkingCalculatorParserTests {
         ("1/2", UncheckedRational(1, 2)),
         ("3 3/4", UncheckedRational(15, 4)),
         ("1  1/2", UncheckedRational(3, 2)),
+        ("1/0", UncheckedRational(1, 0)),
         ("1 /2", nil),
         ("1/ 2", nil),
         ("1 / 2", nil),
@@ -91,53 +92,68 @@ struct WoodworkingCalculatorParserTests {
 // Serialized: Citron is not thread-safe.
 @Suite(.serialized)
 struct EvaluatableCalculationTests {
-    @Test<[(String, EvaluatableCalculation, Quantity)]>("from", arguments: [
+    @Test<[(String, EvaluatableCalculation, Result<Quantity, DivisionByZeroError>)]>("from", arguments: [
         (
             "1/2",
             .rational(UncheckedRational(1, 2)),
-            .rational(rational(1, 2)),
+            .success(.rational(rational(1, 2))),
         ),
         (
             "3.5",
             .real(3.5),
-            .real(3.5),
+            .success(.real(3.5)),
         ),
         (
             "1 + 2 × 3",
             .add(.rational(UncheckedRational(1, 1)), .multiply(.rational(UncheckedRational(2, 1)), .rational(UncheckedRational(3, 1)))),
-            .rational(rational(7, 1)),
+            .success(.rational(rational(7, 1))),
         ),
         (
             "(1 + 2) × 3",
             .multiply(.add(.rational(UncheckedRational(1, 1)), .rational(UncheckedRational(2, 1))), .rational(UncheckedRational(3, 1))),
-            .rational(rational(9, 1)),
+            .success(.rational(rational(9, 1))),
         ),
         (
             "1 + 2 × 3 ÷ 4",
             .add(.rational(UncheckedRational(1, 1)), .divide(.multiply(.rational(UncheckedRational(2, 1)), .rational(UncheckedRational(3, 1))), .rational(UncheckedRational(4, 1)))),
-            .rational(rational(5, 2)),
+            .success(.rational(rational(5, 2))),
         ),
         (
             "1' 3\" × 3.2",
             .multiply(.rational(UncheckedRational(15, 1)), .real(3.2)),
-            .real(48.0),
+            .success(.real(48.0)),
         ),
         (
             "-1",
             .subtract(.rational(UncheckedRational(0, 1)), .rational(UncheckedRational(1, 1))),
-            .rational(rational(-1, 1)),
+            .success(.rational(rational(-1, 1))),
         ),
         (
             "1 -- 2",
             .subtract(.rational(UncheckedRational(1, 1)), .subtract(.rational(UncheckedRational(0, 1)), .rational(UncheckedRational(2, 1)))),
-            .rational(rational(3, 1)),
-        )
-    ]) func from(input: String, expectedEvaluatable: EvaluatableCalculation, expectedResult: Quantity) throws {
+            .success(.rational(rational(3, 1))),
+        ),
+        (
+            "1/0",
+            .rational(UncheckedRational(1, 0)),
+            .failure(DivisionByZeroError())
+        ),
+        (
+            "1 ÷ 0",
+            .divide(.rational(UncheckedRational(1, 0)), .rational(UncheckedRational(0, 0))),
+            .failure(DivisionByZeroError())
+        ),
+        (
+            "1 / (1 - 1)",
+            .divide(.rational(UncheckedRational(1, 0)), .subtract(.rational(UncheckedRational(1, 0)), .rational(UncheckedRational(1, 0)))),
+            .failure(DivisionByZeroError())
+        ),
+    ]) func from(input: String, expectedEvaluatable: EvaluatableCalculation, expectedResult: Result<Quantity, DivisionByZeroError>) throws {
         let evaluatable = EvaluatableCalculation.from(input)
         // FIXME: I would like to do straight equality, but I don't want to give UncheckedRational
         // an equality definition since it does not reduce to lowest terms, etc.
         #expect(evaluatable?.description == expectedEvaluatable.description)
-        #expect(evaluatable!.evaluate() == .success(expectedResult))
+        #expect(evaluatable!.evaluate() == expectedResult)
     }
     
     @Test("from (nil)", arguments: [
